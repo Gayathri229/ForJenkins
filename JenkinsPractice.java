f8c5b8e237a44e6828ab7a4dbfd8d9b0a9fb7e90
Gayathri Arumugam <gayathriarumugam229@gmail.com>
2024-07-19 12:02:09 +0530

    Update JenkinsPractice.java

--- JenkinsPractice.java
+++ JenkinsPractice.java
@@ -2,7 +2,7 @@ class JenkinsPractice {
     public static void main(String[] args) {
         System.out.println("Hey Jenkins");
         System.out.println("From Github");
-        System.out.println("adding for trigger");
+        System.out.println("adding for triggerrr");
 
 
     }
